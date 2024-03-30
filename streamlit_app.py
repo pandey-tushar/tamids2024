c7639b16f3d24b63058690797d7e19a9d5aeecc6
Tushar Pandey <56597687+pandey-tushar@users.noreply.github.com>
2024-03-29 19:33:58 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,8 +10,8 @@ st.title('📊 Interactive Data Explorer')
 with st.expander('About this app'):
   st.markdown('**What can this app do?**')
   st.info('This app shows the use of Pandas for data wrangling, Altair for chart creation and editable dataframe for data interaction.')
-  st.markdown('**How to use the app?**')
-  st.warning('To engage with the app, 1. Select genres of your interest in the drop-down selection box and then 2. Select the year duration from the slider widget. As a result, this should generate an updated editable DataFrame and line plot.')
+  st.markdown('**Impact of Sea Level Rise**')
+  st.warning('This will look at correlation between the sea level rising and different data')
   
 st.subheader('SEA LEVEL CHANGE')
 
