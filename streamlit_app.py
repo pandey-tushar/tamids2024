838709ce37725f144ffaff45a07ec24b4acc10e4
Tushar Pandey <56597687+pandey-tushar@users.noreply.github.com>
2024-04-02 20:24:49 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -81,15 +81,24 @@ def network_analysis():
     st.write(" ")
     st.write("""
             We can go to the network analysis page by clicking
-            on the link. Below is a snapshot of the department-wise Grant
-            network analysis""")
+            on the link. Below is a snapshot of the green house emissions and the sea level rise 
+            in the guld of Mexico""")
 
     #To access the network analysis, press the button below.
 
     st.write("")
     col1, col2, col3 = st.columns((1,1,1))
-    link = '[Green House emissions and sea level rise](https://pandey-tushar.github.io/tamids2024/)'
+    link = '[Green House emissions and sea level rise in Gulf of Mexico](https://pandey-tushar.github.io/tamids2024/)'
     col2.markdown(link, unsafe_allow_html=True)
+
+    st.write(" ")
+    #Setting the Image
+    image = Image.open('Images/gulf_of_mexico_TDA.jpg')
+
+    #Setting the image width
+    st.image(image, use_column_width = True)
+
+    st.write(" ")
 
 
 
@@ -246,10 +255,9 @@ def authors():
 
 
 # Page title
-st.set_page_config(page_title="Sea Level Rising and it's impact", page_icon='📊')
-st.title('📊 Sea Level Rising and impact')
-  
-st.subheader('SEA LEVEL CHANGE')
+st.set_page_config(layout='wide', page_title="Sea Level Rising and it's impact", page_icon='🌊')
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
 
 
 # Sidebar navigation for users -
@@ -285,36 +293,3 @@ elif navigation_tab == 'Controlling sea level rise':
 elif navigation_tab == 'About the Authors':
     authors()
 
-# Load data
-# df = pd.read_csv('data/movies_genres_summary.csv')
-# df.year = df.year.astype('int')
-
-# # Input widgets
-# ## Genres selection
-# genres_list = df.genre.unique()
-# genres_selection = st.multiselect('Select genres', genres_list, ['Action', 'Adventure', 'Biography', 'Comedy', 'Drama', 'Horror'])
-
-# ## Year selection
-# year_list = df.year.unique()
-# year_selection = st.slider('Select year duration', 1986, 2006, (2000, 2016))
-# year_selection_list = list(np.arange(year_selection[0], year_selection[1]+1))
-
-# df_selection = df[df.genre.isin(genres_selection) & df['year'].isin(year_selection_list)]
-# reshaped_df = df_selection.pivot_table(index='year', columns='genre', values='gross', aggfunc='sum', fill_value=0)
-# reshaped_df = reshaped_df.sort_values(by='year', ascending=False)
-
-
-# # Display DataFrame
-
-# df_editor = st.data_editor(reshaped_df, height=212, use_container_width=True,
-#                             column_config={"year": st.column_config.TextColumn("Year")},
-#                             num_rows="dynamic")
-# df_chart = pd.melt(df_editor.reset_index(), id_vars='year', var_name='genre', value_name='gross')
-
-# # Display chart
-# chart = alt.Chart(df_chart).mark_line().encode(
-#             x=alt.X('year:N', title='Year'),
-#             y=alt.Y('gross:Q', title='Gross earnings ($)'),
-#             color='genre:N'
-#             ).properties(height=320)
-# st.altair_chart(chart, use_container_width=True)
